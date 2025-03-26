c92d6ab1de8e6677554259a2aad7153657327fb0
TamQuanThanh <155310063+TamQuanThanh@users.noreply.github.com>
2025-03-26 15:24:00 +0700

    mess2

--- README.md
+++
@@ -1,5 +0,0 @@
-# K8S-Sample
-
-
-
-fasfhdsjfhsdahfsadhjfl
--- terraform/module/ssh-key/output.tf
+++
@@ -1,7 +0,0 @@
-output "private_key" {
-  value = local.is_linux ? tls_private_key.pk[0].private_key_pem : tls_private_key.pk_win[0].private_key_pem
-}
-
-output "public_key" {
-  value = local.is_linux ? tls_private_key.pk[0].public_key_openssh : tls_private_key.pk_win[0].public_key_openssh
-}
